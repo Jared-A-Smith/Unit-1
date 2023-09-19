b41dd9978c4d679b752b1a4e1b18e2149f905ae7
Jared-A-Smith <jaredsmith0626@gmail.com>
2023-09-18 21:14:42 -0500

    Finalize Activity 3, main_with_debug script

--- Chapter02/js/main_with_debug.js
+++ Chapter02/js/main_with_debug.js
@@ -53,46 +53,49 @@ addColumns(cityPop);
 // LOOP TO CREATE NEW COLUMN FOR City Size-------------------------------------------------------------
 console.log('Before forEach Loop');
 function addColumns(cityPop){
+	//create variable which is selecting all table row elements
     var rows = document.querySelectorAll("tr");
     document.querySelectorAll("tr").forEach(function(row, i){
-
+		//if row == 0 create header row with City Size label
 		if (i == 0){
 			
     		row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
-			// table.appendChild(row);
+		// if row does not == 0, run else, if, and else if statements to assign city size
     	} else {
-
+			//create citySize variabe to be populated by for each loop and to be used in insertAdjacentHTML
     		var citySize;
-
+			//if cityPop is less than 100000 assign "small"
     		if (cityPop[i-1].population < 100000){
     			citySize = 'Small';
-
+			//if cityPop is less than 500000 assign "medium"
     		} else if (cityPop[i-1].population < 500000){
     			citySize = 'Medium';
-
+			//if cityPop is greater than 500000 assign "large"
     		} else {
     			citySize = 'Large';
     		};
-
-			// var rowHtml = '<tr><td>' + citySize + '</td></tr>';
+			//console.log used check work
 			console.log(citySize);
+			//insert "row" result into the table beneath citySize header
 			row.insertAdjacentHTML("beforeend",'<td>' + citySize + '</td>');
-			// document.querySelector("#mydiv".appendChild(table));
     	};
 		addEvents();
     });
-	// document.querySelector("#mydiv").appendChild(row);
 };
+//console.log used to check work
 console.log("After forEach Loop");
-
+//console.log used to check work
 console.log("Before Event")
+// Create addEvents function listening for mousover action 
 function addEvents(){
-
+	//Select table element and add mouseover event listener with function to hange color as you mouse over the table.
 	document.querySelector("table").addEventListener("mouseover", function(){
+		//craete color variable
 		var color;
+		//assign default rgb value to color variable
 		var color = "rgb(";
 		
-		// var color = "rgb(" + random + "," + random + "," + random + ")";
+		//loop through color variable assigning random colors to the table element
 		for (var i=0; i<3; i++){
 
 			var random = Math.round(Math.random() * 255);
@@ -106,25 +109,17 @@ function addEvents(){
 				color += ")";
 				}
     		};
+		//console.log used to check work
 		console.log(color);
+		//apply style to table
 		document.querySelector("table").style.color = color;
+		//create click me event with an alert notifying user that they clicked feature
+		document.querySelector("table").addEventListener("click", function(){
+			alert('Hey, you clicked me!');
+		})
 		});
-		// console.log(color);
-		// document.querySelector("table").style.color = color;
-	console.log("After Event")
-
-		// document.querySelector("table").color = color;;
-    
-	console.log("before click me")
-	function clickme(){
-
-		alert('Hey, you clicked me!');
-		document.querySelector("table").addEventListener("click", clickme);
-	};
+	//console.log to check work
 	console.log("after click me")
-	// table.addEventListener("click", clickme);
 };
-
-	// document.querySelector("table").addEventListener("click", clickme);
 
 document.addEventListener('DOMContentLoaded', initialize);
